968204d7421d3a7b6c08db129b3cc9b470ff1c8d
ChristianGroeber <christian.groeber@bluewin.ch>
2021-12-15 17:30:08 +0100

    logging ip addresses to prevent spam

--- pages/send-message.php
+++ pages/send-message.php
@@ -4,7 +4,7 @@ header('content-type: application/json');
 
 if (strtolower($_SERVER['REQUEST_METHOD']) !== 'post') {
     http_response_code(405);
-    echo json_encode(['error' => 'Only POST is allowed']);
+    echo json_encode(['error' => 'Only POST is allowed', $_SERVER, getallheaders()]);
     die();
 }
 
@@ -32,6 +32,33 @@ if ($messageTooLong) {
     die();
 }
 
+$ipsPath = $_SERVER['DOCUMENT_ROOT'] . '/contact_ips.json';
+if (!file_exists($ipsPath)) {
+    file_put_contents($ipsPath, json_encode([]));
+}
+
+$ips = json_decode(file_get_contents($ipsPath), true);
+
+$ip = $_SERVER['HTTP_CLIENT_IP']
+    ? $_SERVER['HTTP_CLIENT_IP']
+    : ($_SERVER['HTTP_X_FORWARDED_FOR']
+        ? $_SERVER['HTTP_X_FORWARDED_FOR']
+        : $_SERVER['REMOTE_ADDR']);
+
+$now = new DateTime();
+
+if (key_exists($ip, $ips)) {
+    $dateLastRequest = new DateTime($ips[$ip]);
+    $now->modify('-1 day');
+    $diff = $now->diff($dateLastRequest);
+
+    if ($diff->invert === 0) {
+        http_response_code(429);
+        echo json_encode(['error' => 'You are only allowed one message per day']);
+        die();
+    }
+}
+$now = new DateTime();
 
 $msg = $message;
 $msg .= "\n\nFrom: " . $name;
@@ -39,6 +66,9 @@ $msg .= "\n\nE-Mail: " . $email;
 
 // $success = mail('christian.groeber@bluewin.ch', "New Contact Entry by " . $_REQUEST['contact_name'], $message, ['from', 'christian.groeber@nxtlvl.ch']);
 $success = true;
+
+$ips[$ip] = $now->format('Y-m-d h:i:s');
+file_put_contents($ipsPath, json_encode($ips));
 
 $ret = ['success' => $success];
 
